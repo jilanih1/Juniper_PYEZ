2d3c4a64aa405924aaca7b5f2a6d0c4fc15d167f
Hammad <jilanih1@gmail.com>
2017-11-08 14:10:31 -0600

    modified:   collect_jlogs.py if __name__ == '__main__':

--- collect_jlogs.py
+++ collect_jlogs.py
@@ -8,36 +8,39 @@ from jnpr.junos import Device
 from jnpr.junos.exception import ConnectError
 from jnpr.junos.utils.start_shell import StartShell
 
-if sys.version_info[:2] <= (2, 7):
-	input = raw_input
+if __name__ == '__main__':
 
-#hostname = input('Device hostname: ')
-#username = input('Device username: ')
-password = getpass.getpass('Device password: ')
-hostname = '192.168.1.48'
-username = 'admin'
+	if sys.version_info[:2] <= (2, 7):
+		input = raw_input
 
-dev = Device(host=hostname, user=username, passwd=password)
+	#hostname = input('Device hostname: ')
+	#username = input('Device username: ')
+	password = getpass.getpass('Device password: ')
+	hostname = '192.168.1.48'
+	username = 'admin'
 
-try:
-	dev.open()
-except ConnectError as err:
-	print('Cannot connect to device: {0}'.format(err))
-	sys.exit(1)
-except Exception as err:
-	print(err)
-	sys.exit(1)
+	dev = Device(host=hostname, user=username, passwd=password)
 
-jshell = StartShell(dev)
-jshell.open()
-jshell.run('cli -c "request support information | save /var/tmp/PYEZ_RSI.txt"')
-jshell.run('tar -zcvf /var/tmp/PYEZ_varlog.tar.gz /var/log/*')
-#flist = json.dumps(jshell.run('cli -c "file list /var/tmp | grep PYEZ"'))
+	try:
+		dev.open()
+	except ConnectError as err:
+		print('Cannot connect to device: {0}'.format(err))
+		sys.exit(1)
+	except Exception as err:
+		print(err)
+		sys.exit(1)
 
-flist = jshell.run('cli -c "file list /var/tmp | grep PYEZ" ')
-for line in flist:
-	print(line)
-jshell.close()
-dev.close()
+	jshell = StartShell(dev)
+	jshell.open()
+	jshell.run('cli -c "request support information | save /var/tmp/PYEZ_RSI.txt"')
+	jshell.run('tar -zcvf /var/tmp/PYEZ_varlog.tar.gz /var/log/*')
+	#flist = json.dumps(jshell.run('cli -c "file list /var/tmp | grep PYEZ"'))
+
+	flist = jshell.run('cli -c "file list /var/tmp | grep PYEZ" ')
+	for line in flist:
+		print(line)
+	jshell.close()
+	dev.close()
+	
 sys.exit(1)
 
